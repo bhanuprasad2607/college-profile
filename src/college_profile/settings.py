0ba758932b9a44ec40c54c8cd3d2b13451f35c03
bhanuprasad2607 <bhanuprasad2607@github.com>
2021-07-09 08:04:00 +0530

    initial commit

--- src/college_profile/settings.py
+++ src/college_profile/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # User defined application for project
+    'login',
+    'college_admin',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,9 @@ ROOT_URLCONF = 'college_profile.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
